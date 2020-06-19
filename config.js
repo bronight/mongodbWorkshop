e8573b177592e3b18467c452ad267daec2cc1e48
Louis Williams <louawilliams@outlook.com>
2020-06-18 23:08:52 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '575481749778118',
+        clientSecret: '04e71070b6f72c5ff3afee1432a58009'
+    }
 }
